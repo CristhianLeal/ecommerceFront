ce4fd2db1bad179bcc4204f7840cd5ef4aac1924
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-05 13:54:24 -0300

    creation of barrel for components and pages

---
+++ src/Pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home/Home'
+export { default as DetailPage } from './DetailPage/DetailPage'
+export { default as RegisterProduct } from './RegisterProduct/RegisterProduct'
+export { default as AdminProducts } from './AdminProducts/AdminProducts'
