0e1a0622588aee7ed05b8b23388c872e7381a76b
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-05 13:51:08 -0300

    creation of the project eCommerFront

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1 align="center">
+Challenge 
+</h1>
+
+<div align="center">
+
+![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white) 
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+![ReactRouter](https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) 
+![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+![Eslint](https://img.shields.io/badge/eslint-3A33D1?style=for-the-badge&logo=eslint&logoColor=white) 
+
+</div>
---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
+// import { ToastContainer } from 'react-toastify'
+import './App.css'
+import 'react-toastify/dist/ReactToastify.css'
+import { Home, DetailPage, RegisterProduct, AdminProducts } from './Pages'
+import { Navbar } from './Components'
+
+function App () {
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/detailpage/:id' element={<DetailPage />} />
+        <Route path='/registerproduct' element={<RegisterProduct />} />
+        <Route path='/adminproducts' element={<AdminProducts />} />
+      </Routes>
+      {/* <ToastContainer
+        position="top-right"
+        autoClose={2000}
+      /> */}
+    </>
+  )
+}
+export default App
