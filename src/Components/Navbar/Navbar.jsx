6bba926005b5217d9538e83ddae45a38c618edc6
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-06 21:51:37 -0300

    updateing navbar with loging

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,44 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-// import logo from '../../assets/GamalDigitalColor.png'
+import logo from '../../assets/logo.png'
 import './navbar.css'
+import axios from 'axios'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
-  const [user, setUser] = useState('')
+  const [loged, setLoged] = useState(false)
   const toggleNavbar = () => {
     setIsOpen(!isOpen)
   }
   const closeNavbar = () => {
     setIsOpen(false)
   }
-  const handleClick = () => {
-    sessionStorage.clear()
-    window.location.href = '/'
+  const loging = () => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:8003/products/log')
+        sessionStorage.setItem('token', response.data.token)
+        console.log(response)
+      } catch (error) {
+        console.error('Error al obtener token', error)
+      }
+    }
+    fetchProducts()
+    closeNavbar()
+    setLoged(!loged)
+    fetchProducts()
   }
-  useEffect(() => {
-    setUser(sessionStorage.getItem('user'))
-  }, [])
+  const closing = () => {
+    sessionStorage.clear()
+    closeNavbar()
+    setLoged(!loged)
+  }
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top text-white navTotal">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top text-white navTotal py-2">
       <div className="container d-flex justify-content-between align-items-center">
         <Link className="navbar-brand text-white" to="/">
-          {/* <img src={logo} alt="logo" className='logoNav' /> */}
+          <img src={logo} alt="logo" className='logoNav' />
         </Link>
         <button className="navbar-toggler colorSpan" type="button" onClick={toggleNavbar}>
           <span className="navbar-toggler-icon colorSpan"></span>
@@ -33,24 +47,22 @@ const Navbar = () => {
           id="navbarNav">
           <ul className="navbar-nav ml-auto d-flex align-items-end gap-4">
             <li className="nav-item">
-            <Link className="nav-link text-white" to= "/mainpage" onClick={closeNavbar}>Trabajos</Link>
+            <Link className="nav-link text-white" to= "/" onClick={closeNavbar}>Productos</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/aboutus" onClick={closeNavbar}>Nosotros</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/contact" onClick={closeNavbar}>Contacto</Link>
-            </li>
+            { loged === true &&
+              <li className="nav-item">
+              <Link className="nav-link text-white" to="/adminproducts" onClick={closeNavbar}>Adminitración</Link>
+            </li>}
             {
-              user !== null &&
+              loged === false &&
                 <li className=''>
-                  <Link className="nav-link text-white" to="/admin" onClick={closeNavbar}>Administración</Link>
+                  <Link className="nav-link text-white" onClick={loging}>Logeate</Link>
                 </li>
             }
             {
-              user !== null &&
+              loged === true &&
                 <li>
-                  <Link className="nav-link text-white" onClick={handleClick}>Cerrar sesión</Link>
+                  <Link className="nav-link text-white" onClick={closing}>Cerrar sesión</Link>
                 </li>
             }
           </ul>
