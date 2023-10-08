f242aaa93cd7b0cc39043f69cc9b4ac307c67f38
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-08 19:34:39 -0300

    changes on style of the card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -5,7 +5,9 @@ const Card = ({ product }) => {
   return (
     <div className="card border-0">
       <Link className=' text-decoration-none' to={`/detailpage/${product._id}`}>
-        <img src={product.imageUrl} className="card-img-top img-fluid border-0" alt="Imagen" />
+        <div className='text-center'>
+          <img src={product.imageUrl} className="card-img-top img-fluid border-0" alt="Imagen" />
+        </div>
         <div className="card-body">
           <h5 className="card-title text-black text-center">{product.name}</h5>
           <p className="card-text text-black text-center">Precio:$ {product.price}</p>
