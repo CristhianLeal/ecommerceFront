1d62477eb288f69b908eeb6efe3e0c0f6e672f37
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-10 18:52:50 -0300

    Table with spinner and update with back

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,10 +1,30 @@
 import { Link } from 'react-router-dom'
 import { useProducts } from '../../hooks/useProducts'
+import { useState, useEffect } from 'react'
 import './table.css'
 const Table = () => {
   const { productsData, deleteProduct, editProduct } = useProducts()
+  const [loading, setLoading] = useState(true)
+  const deleting = (id) => {
+    setLoading(id)
+    deleteProduct(id)
+  }
+  useEffect(() => {
+    if (productsData.length > 0) {
+      setLoading([])
+    }
+  }, [productsData])
   return (
     <div>
+      {loading === true
+        ? <div className='d-flex flex-row align-items-center justify-content-center'>
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden"></span>
+            </div>
+              <p className='d-flex justify-content-center align-items-center m-0 py-0 px-3'>Cargando productos...</p>
+          </div>
+        : ''
+      }
       <table className="table table-dark table-bordered">
         <thead>
           <tr>
@@ -24,7 +44,7 @@ const Table = () => {
         </thead>
         <tbody>
           {productsData?.map((product) => (
-            <tr key={product._id}>
+            <tr key={product.id}>
               <td>
                 <p className='m-0'>{product.name}</p>
               </td>
@@ -38,9 +58,18 @@ const Table = () => {
                 <p className='m-0'>{product.price}</p>
               </td>
               <td className='text-center'>
-                <button className="btn btn-danger action-button" onClick={() => deleteProduct(product._id) }>
-                  <i className="bi bi-trash">Eliminar</i>
-                </button>
+              {loading !== product.id
+                ? <button className="btn btn-danger action-button" onClick={() => deleting(product.id) }>
+                    <i className="bi bi-trash">Eliminar</i>
+                  </button>
+                : ''
+              }
+              {loading === product.id
+                ? <div className="spinner-border" role="status">
+                    <span className="visually-hidden"></span>
+                  </div>
+                : ''
+              }
                 <Link className='text-decoration-none text-white' to={'/registerproduct'} onClick={() => editProduct(product)}>
                   <button className="btn btn-success action-button">
                     <i className="bi bi-pen">Editar</i>
