86b9ba52c44f20e4083935a8d2777300009913ee
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-10 18:54:49 -0300

    Detail update with spinner

--- src/Pages/DetailPage/DetailPage.jsx
+++ src/Pages/DetailPage/DetailPage.jsx
@@ -10,6 +10,7 @@ const DetailPage = () => {
   const [visible, setVisible] = useState([])
   const [showFullScreenImage, setShowFullScreenImage] = useState(false)
   const [photo, setPhoto] = useState(false)
+  const [loading, setLoading] = useState(true)
   const ERef0 = useRef(null)
   const handleScroll = () => {
     const refs = [ERef0]
@@ -35,8 +36,23 @@ const DetailPage = () => {
     }
   }, [])
 
+  useEffect(() => {
+    if (productData.id > 0) {
+      setLoading(false)
+    }
+  }, [productData])
+
   return (
     <div className='conten'>
+      {loading === true
+        ? <div className='d-flex flex-row align-items-center justify-content-center'>
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden"></span>
+            </div>
+              <p className='d-flex justify-content-center align-items-center m-0 py-0 px-3'>Cargando productos...</p>
+          </div>
+        : ''
+      }
       <div ref={ERef0} className={`Efectd1 ${visible[0] ? 'EfectVisibled1' : ''}`}>
         <h3 className='text-black text-center titleDetail mt-4'>{productData?.name}</h3>
         <div className='d-flex flex-column flex-md-row justify-content-center align-items-center gap-0 gap-lg-5 pt-2 pb-3'>
